a814e22fd1c8db60ec7b7bb0c147ff4f43d7df84
Roy <oisroy233@gmail.com>
2022-05-22 07:40:35 +0800

    转移统计代码 避免将隐私信息统计进去

--- handler/IndexHandler.go
+++ handler/IndexHandler.go
@@ -1,12 +1,9 @@
 package handler
 
 import (
-	"bytes"
 	"github.com/gin-gonic/gin"
-	"github.com/rroy233/EnterNEU/configs"
 	"github.com/rroy233/EnterNEU/utils"
 	"io/ioutil"
-	"strings"
 )
 
 func IndexHandler(c *gin.Context) {
@@ -14,16 +11,6 @@ func IndexHandler(c *gin.Context) {
 	if err != nil {
 		utils.ReturnPlainHtml(c, "模板读取失败")
 		return
-	}
-
-	//判断是否需要插入统计代码
-	//请在config.yaml中设置，本项目默认采用https_v6_51_la提供的统计服务
-	if configs.Get().StatisticsReport.V651La == true {
-		jss := strings.Join([]string{configs.Get().StatisticsReport.V651LaJs1,
-			configs.Get().StatisticsReport.V651LaJs2,
-			"</head>",
-		}, "")
-		index = bytes.Replace(index, []byte("</head>"), []byte(jss), 1)
 	}
 
 	c.Data(200, "text/html;charset=utf-8", index)
